65df440b747df167dfd69540353b81c20d04abc0
Alan Franco <alanfrancoj@gmail.com>
2019-04-01 20:03:56 -0600

    Movimiento de ajolotes y arbustos
    Creados scripts (no terminados) de movimiento de ajolotes y arbustos

---
+++ Assets/Scripts/Arbusto.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arbusto : MonoBehaviour
+{
+
+    Animator m_animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m_animator.Play("Efecto");
+            Debug.Log("Si colisiona");
+        }
+    }
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Player"))
+        {
+            m_animator.Play("Efecto");
+        }
+    }*/
+}
---
+++ Assets/Scripts/PatrullaAjolote.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrullaAjolote : MonoBehaviour
+{
+    public string Movimiento, Asustado;
+
+    public static bool harto, movimiento, asustado;
+
+    public float velocidad = 5f;
+    public float cambioDireccion = 1f;
+    public float maxCambio = 30f;
+
+    Animator ajoloteAnimator;
+
+    CharacterController controller;
+    float direccion;
+    Vector3 rotacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ajoloteAnimator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+
+        direccion = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, direccion, 0);
+
+        StartCoroutine(NuevaDireccion());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!harto)
+        {
+            ajoloteAnimator.SetBool(Movimiento,true);
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, rotacion, Time.deltaTime * cambioDireccion);
+            Vector3 adelante = transform.TransformDirection(Vector3.forward);
+            controller.SimpleMove(adelante * velocidad);
+        }
+        
+    }
+
+    IEnumerator NuevaDireccion()
+    {
+        while (true)
+        {
+            NuevaDireccionRutina();
+            yield return new WaitForSeconds(cambioDireccion);
+        }
+    }
+
+    void NuevaDireccionRutina()
+    {
+        float piso = transform.eulerAngles.y - maxCambio;
+        float techo = transform.eulerAngles.y + maxCambio;
+        direccion = Random.Range(piso, techo);
+        rotacion = new Vector3(0, direccion, 0);
+    }
+}
