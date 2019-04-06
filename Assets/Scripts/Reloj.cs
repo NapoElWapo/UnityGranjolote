254c9b34e4e6341f4f7d2ebea088790e4116037f
Cuajo <saulnharoh@gmail.com>
2019-04-05 19:53:39 -0600

    Ciclo dia y noche agregado junto con hora actual del dia
    El ciclo de dia y noche funciona dependiendo del valor que tenga el texto de Horas y minutos

---
+++ Assets/Scripts/Reloj.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class Reloj : MonoBehaviour
+{
+    private Text textClock;
+    public int hora;
+    public float minuto;
+    public float tiempoTotal;
+    public Transform solTransform;
+    public Transform lunaTransform;
+
+    void Start()
+    {
+        textClock = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        minuto += Time.deltaTime;
+        
+        if(hora==0)
+        {
+            tiempoTotal = minuto;
+        }
+        else
+        {
+            tiempoTotal = (hora*60) + minuto;
+        }
+        
+
+        if(minuto>=60)
+        {
+            hora = hora + 1;
+            minuto = 0;
+            
+        }
+
+        if(hora >23)
+        {
+            hora = 0;
+        }
+
+
+        if(minuto>10&&hora<10)
+        {
+            textClock.text = "0"+hora + ":" + (int)minuto;
+        }
+        else if(hora>9 && minuto<10)
+        {
+            textClock.text = hora + ":0" + (int)minuto;
+        }
+        else if(hora >= 10 && minuto >= 10)
+        {
+            textClock.text = hora + ":" + (int)minuto;
+        }
+        else
+        {
+            textClock.text = "0" + hora + ":0" + (int)minuto;
+        }
+
+        solTransform.rotation = Quaternion.Euler(new Vector3((tiempoTotal*0.25f)-90,0,0));
+        lunaTransform.rotation = Quaternion.Euler(new Vector3((tiempoTotal * 0.25f)+90, 0, 0));
+    }
+
+    
+}
