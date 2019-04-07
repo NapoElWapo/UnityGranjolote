09bac85869d001801fa032806b03e537a795c855
Alan Franco <alanfrancoj@gmail.com>
2019-04-07 16:58:40 -0500

    Script de spawn de ajolotes
    -Creado script basico de spawneo de ajolotes
    -(Error) Al crear los prefabs de los ajolotes se jodieron las dimensiones

---
+++ Assets/Scripts/SpawnAjolote.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAjolote : MonoBehaviour
+{
+    public GameObject ajolote;
+    public float tiempoSpawn = 15f;
+    public int porcentajeSpawn = 30;
+    public int Spawnea;
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", tiempoSpawn, tiempoSpawn);
+    }
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        Spawnea = Random.Range(1, 100);
+        if (Spawnea <= porcentajeSpawn)
+        {
+            Instantiate(ajolote, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        }
+    }
+
+}
