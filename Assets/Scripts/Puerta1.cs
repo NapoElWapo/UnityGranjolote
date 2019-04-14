c9921684f0ef7b7eba456307d927c8c57788fb9f
JetCourd <luisferaguirre99@gmail.com>
2019-04-13 19:33:16 -0500

    Puertas
    Puertas ya funcionan, llevana  una escena en especifico

---
+++ Assets/Scripts/Puerta1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta1 : MonoBehaviour
+{
+    public string levelToLoad;
+    void Start()
+    {
+        
+    }
+   void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
