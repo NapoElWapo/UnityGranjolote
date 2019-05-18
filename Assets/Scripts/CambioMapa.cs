99048bbbf0db4d08e4d7ec863d3b5c30329c5d9f
Alan Franco <alanfrancoj@gmail.com>
2019-05-18 14:11:18 -0500

    Mapa funcionando en la escena principal
    -(Error): El inventario no permite hacer clic en los botones para cambiar pestañas

--- Assets/Scripts/CambioMapa.cs
+++ Assets/Scripts/CambioMapa.cs
@@ -20,66 +20,50 @@ public class CambioMapa : MonoBehaviour
     {
         if (this.tag == "Pueblo" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(-257f, 43f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Planicie" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(-40f, 43f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Bosque" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(-28f, -50f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Cueva" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(136f, -50f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Rocas" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(139f, 43f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Volcan" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(293f, 43f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Nevada" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(364f, 68f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
 
         if (this.tag == "Nube" && other.tag == "Player")
         {
-            Debug.Log(imagenMapa.localPosition);
             imagenMapa.localPosition = new Vector2(329f, -62f);
             Debug.Log("Cambio de zona a " + this.tag);
-            Debug.Log(imagenMapa.localPosition);
         }
     }
 }
