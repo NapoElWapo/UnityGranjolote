1b10b13d7ca01d5e381403fd29ea633412229459
Alan Franco <alanfrancoj@gmail.com>
2019-05-18 11:42:40 -0500

    Sistema de mapa casi completo
    -Creado script para que al cambiar de zona se actualize el mapa del inventario

---
+++ Assets/Scripts/CambioMapa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class CambioMapa : MonoBehaviour
+{
+    public RectTransform imagenMapa;
+    BoxCollider boxCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+        imagenMapa = GameObject.Find("JugadorMapa").GetComponent<RectTransform>();
+        boxCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (this.tag == "Bosque" && other.tag == "Player")
+        {
+            Debug.Log(imagenMapa.transform.position);
+            imagenMapa.position = new Vector2(490f, 275f);
+            Debug.Log("Cambio de zona");
+            Debug.Log(imagenMapa.transform.position);
+        }
+    }
+}
