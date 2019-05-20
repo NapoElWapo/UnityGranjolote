58e8e24cf452121a646a92d43c4afe9a27bf92e9
Cuajo <saulnharoh@gmail.com>
2019-05-20 13:16:44 -0500

    UI jugador

---
+++ Assets/Scripts/HerramientaSeleccionada.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HerramientaSeleccionada : MonoBehaviour
+{
+    public RectTransform bslot1, bslot2, bslot3, bslot4;
+    public Image sslot1, sslot2, sslot3, sslot4;
+    public GameObject lanza, rastrillo, ajoloteSA, ajoloteSF;
+    private int slotPosition=0;
+    MenuInventario conex;
+    [SerializeField]
+    slot comprobar;
+    
+    void Start()
+    {
+        conex = GameObject.Find("InventarioUI").GetComponent<MenuInventario>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CambiarSlotActivo();
+        Debug.Log(slotPosition);
+    }
+
+    private void CambiarSlotActivo()
+    {
+        if(Input.GetButtonDown("r"))
+        {
+            slotPosition--;
+        }
+        else if(Input.GetButtonDown("t"))
+        {
+            slotPosition++;
+        }
+
+        if(slotPosition<0)
+        {
+            slotPosition = 3;
+        }
+        else if(slotPosition>3)
+        {
+            slotPosition = 0;
+        }
+
+        switch (slotPosition)
+        {
+            case 0:
+
+                bslot1.gameObject.SetActive(true);
+                bslot2.gameObject.SetActive(false);
+                bslot3.gameObject.SetActive(false);
+                bslot4.gameObject.SetActive(false);
+                rastrillo.gameObject.SetActive(false);
+                ajoloteSA.gameObject.SetActive(false);
+                ajoloteSF.gameObject.SetActive(false);
+
+                comprobar = conex.herramientas.Find(x => x.Slot_name == "Slot1");
+               
+                if (comprobar.RealItemName=="Lanza")
+                {
+                    sslot1.sprite = comprobar.Slot_ui_img.sprite;
+                    lanza.gameObject.SetActive(true);
+                    
+                }
+
+               
+
+                
+                
+
+                break;
+
+            case 1:
+
+
+
+                comprobar = conex.herramientas.Find(x => x.Slot_name == "Slot2");
+
+                if (comprobar.RealItemName == "Rastrillo")
+                {
+                    sslot2.sprite = comprobar.Slot_ui_img.sprite;
+                    rastrillo.gameObject.SetActive(true);
+
+                }
+
+
+
+               
+                bslot1.gameObject.SetActive(false);
+                bslot2.gameObject.SetActive(true);
+                bslot3.gameObject.SetActive(false);
+                bslot4.gameObject.SetActive(false);
+                lanza.gameObject.SetActive(false);
+                
+                ajoloteSA.gameObject.SetActive(false);
+                ajoloteSF.gameObject.SetActive(false);
+
+                break;
+
+            case 2:
+
+                comprobar = conex.herramientas.Find(x => x.Slot_name == "Slot3");
+
+                if (comprobar.RealItemName == "AjoloteSAgua")
+                {
+                    sslot3.sprite = comprobar.Slot_ui_img.sprite;
+                    ajoloteSA.gameObject.SetActive(true);
+
+                }
+                bslot1.gameObject.SetActive(false);
+                bslot2.gameObject.SetActive(false);
+                bslot3.gameObject.SetActive(true);
+                bslot4.gameObject.SetActive(false);
+                lanza.gameObject.SetActive(false);
+                rastrillo.gameObject.SetActive(false);
+                
+                ajoloteSF.gameObject.SetActive(false);
+
+                break;
+
+            case 3:
+
+                comprobar = conex.herramientas.Find(x => x.Slot_name == "Slot4");
+                Debug.Log(comprobar.Slot_ui_img);
+
+                if (comprobar.RealItemName == "AjoloteSFuego")
+                {
+                    sslot4.sprite = comprobar.Slot_ui_img.sprite;
+                    ajoloteSF.gameObject.SetActive(true);
+
+                }
+                bslot1.gameObject.SetActive(false);
+                bslot2.gameObject.SetActive(false);
+                bslot3.gameObject.SetActive(false);
+                bslot4.gameObject.SetActive(true);
+                lanza.gameObject.SetActive(false);
+                rastrillo.gameObject.SetActive(false);
+                ajoloteSA.gameObject.SetActive(false);
+                
+
+                break;
+
+            
+        }
+    }
+}
