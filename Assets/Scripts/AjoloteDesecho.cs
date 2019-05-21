6144d47d708d6866bcf0c66b3a3cbb18e3508004
Alan Franco <alanfrancoj@gmail.com>
2019-05-20 21:45:12 -0500

    Criadero casi funcional (Modo debug)
    -Interfaz de criadero creada
    -Botones de dejar y recoger ajolote funcionando
    -(Error) El boton que deberia hacer que se recoja el desecho no funciona, desactiva todo el GameObject

---
+++ Assets/Scripts/AjoloteDesecho.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AjoloteDesecho : MonoBehaviour
+{
+
+    public RectTransform boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boton = GetComponent<RectTransform>();
+        StartCoroutine(GenerarDesecho());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GenerarDesecho()
+    {
+        yield return new WaitForSeconds(4);
+        boton.gameObject.SetActive(true);
+        StopCoroutine(GenerarDesecho());
+    }
+
+    public void recogerDesecho()
+    {
+        boton.gameObject.SetActive(false);
+        StartCoroutine(GenerarDesecho());
+    }
+}
