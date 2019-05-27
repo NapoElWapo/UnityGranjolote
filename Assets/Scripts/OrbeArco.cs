bc2a31a29662916c50efa166929733bbe026591c
Cuajo <saulnharoh@gmail.com>
2019-05-26 19:50:39 -0500

    orbe con arco implementado

---
+++ Assets/Scripts/OrbeArco.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbeArco : MonoBehaviour
+{
+
+    public GameObject orbeFuego;
+    public bool pescarConArco=false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+            if (other.tag == "Agua2")
+            {
+
+            orbeFuego.gameObject.SetActive(false);
+            pescarConArco = true;
+            }
+        
+        
+    }
+
+    
+}
