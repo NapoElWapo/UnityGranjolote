7639d13fd765150a7102c244445fdebdda0ce14c
Cuajo <saulnharoh@gmail.com>
2019-06-03 13:19:02 -0500

    Update Hielo.cs

--- Assets/Scripts/Hielo.cs
+++ Assets/Scripts/Hielo.cs
@@ -11,12 +11,12 @@ public class Hielo : MonoBehaviour
     {
             if (other.tag == "Agua"||other.tag=="Agua2")
             {
-                hielo.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+                hielo.transform.localScale += new Vector3(0.005f, 0.005f, 0.005f);
                 hielo.transform.position += new Vector3(0, 0.005f, 0);
             }
             else if (other.tag == "Fuego" || other.tag == "Fuego2")
             {
-                hielo.transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+                hielo.transform.localScale -= new Vector3(0.005f, 0.005f, 0.005f);
                 hielo.transform.position -= new Vector3(0, 0.005f, 0);
             }      
     }
